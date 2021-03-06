23b901dbaaf874399deb62f959161d5d8c2a5c09
Nikita <warhammer-ss@yandex.ru>
2021-03-06 21:40:18 +0300

    fix init paint hashtag color

--- src/content_scripts/color.ts
+++ src/content_scripts/color.ts
@@ -1,7 +1,7 @@
 import type { IStorage } from '../common/storage';
 
 export function paintColorHashtagLine(container: HTMLElement, colors: IStorage['colors']) {
-    const tags = document.querySelectorAll<HTMLElement>('.contentTag');
+    const tags = container.querySelectorAll<HTMLElement>('.contentTag');
 
     for (const tag of tags) {
         const color = colors.find((c) => `#${c.hashtag}` === tag.innerText);
